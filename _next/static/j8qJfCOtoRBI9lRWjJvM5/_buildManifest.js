82c5de98aef5d7ef5067ad8cf80fcc79def9f804
Patricio López Juri <lopezjuripatricio@gmail.com>
2020-11-29 15:50:32 -0300

    Updates

---
+++ _next/static/j8qJfCOtoRBI9lRWjJvM5/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-0aac18857eab3c87a2d5.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-103a71d4ed1e30e24443.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
